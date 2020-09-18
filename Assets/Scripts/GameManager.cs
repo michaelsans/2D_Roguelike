4fbf250efead309ff6188b688671a34f2eb582ea
michaelsans <michaelsansao@hotmail.com>
2020-09-18 13:35:27 -0300

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public float levelStartDelay = .1f;
+    public float turnDelay = .1f;
+    public static GameManager instance = null;
+    public BoardManager boardscript;
+    public int playerHealthPoints = 100;
+    public int playerPoints = 0;
+    [HideInInspector]public bool playersTurn = true;
+
+    private Text levelText;
+    private GameObject levelImage;
+    private int level = 3;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
+    private bool doingSetup;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+        enemies = new List<Enemy>();
+        boardscript = GetComponent<BoardManager>();
+        InitGame();
+    }
+    private void OnLevelWasLoaded(int index)
+    {
+        
+        InitGame();
+        level++;
+    }
+    void InitGame()
+    {
+        doingSetup = true;
+        levelImage = GameObject.Find("levelImage");
+        levelText = GameObject.Find("LevelText").GetComponent<Text>();
+        levelText.text = "Floor " + level;
+        levelImage.SetActive(true);
+        Invoke("HideLevelImage", levelStartDelay);
+
+        enemies.Clear();
+        boardscript.SetupScene(level);
+    }
+    private void HideLevelImage()
+    {
+        levelImage.SetActive(false);
+        doingSetup = false;
+    }
+    public  void GameOver()
+    {
+        levelText.text = "After reaching floor" + level + " you died.";
+        levelImage.SetActive(true);
+        enabled = false;
+    }
+    void Update()
+    {
+        if (playersTurn || enemiesMoving)
+            return;
+        StartCoroutine(MoveEnemies());
+    }
+    public void AddEnemyToList(Enemy script)
+    {
+        enemies.Add(script);
+    }
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if (enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+        playersTurn = true;
+        enemiesMoving = false;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Player : MovingObject
+{
+    public int wallDamage = 1;
+    public int pointPerBarril;
+    public int pointPerPotion;
+    public int health;
+    public float restartLevelDelay = .1f;
+    public Text HP;
+    public Text Points;
+
+    private Animator anim;
+    private int points;
+
+    protected override void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        points = GameManager.instance.playerPoints;
+        health = GameManager.instance.playerHealthPoints;
+        HP.text = "HP: " + health;
+        base.Start();
+    }
+
+    private void OnDisable()
+    {
+        GameManager.instance.playerPoints = points;
+    }
+    void Update()
+    {
+        if (!GameManager.instance.playersTurn) return;
+
+        int horizontal = 0;
+        int vertical = 0;
+
+        horizontal = (int)Input.GetAxisRaw("Horizontal");
+        vertical = (int)Input.GetAxisRaw("Vertical");
+
+        if (horizontal != 0)
+            vertical = 0;
+
+        if (horizontal != 0 || vertical != 0)
+            AttemptMove<Walls>(horizontal, vertical);
+    }
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    {
+        base.AttemptMove<T>(xDir, yDir);
+        RaycastHit2D hit;
+        if (Move(xDir, yDir, out hit))
+        {
+            //Call RandomizeSfx of SoundManager to play the move sound, passing in two audio clips to choose from.
+        }
+        CheckIfGameOver();
+        
+        GameManager.instance.playersTurn = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Escada")
+        {
+            Invoke("Restart", restartLevelDelay);
+            enabled = false;
+        }
+        else if(other.tag == "Barril")
+        {
+            points += pointPerBarril;
+            Points.text = "Points: " + pointPerBarril;
+            other.gameObject.SetActive(false);
+        }
+        else if(other.tag == "Pocao")
+        {
+            points += pointPerPotion;
+            Points.text = "Points: " + pointPerPotion;
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    protected override void OnCantMove<T>(T component)
+    {
+        Walls hitWall = component as Walls;
+        hitWall.DamageWall(wallDamage);
+        anim.SetTrigger("Chop");
+    }
+
+    [System.Obsolete]
+    private void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void loseHP (int loss)
+    {
+        anim.SetTrigger("hit");
+
+        health -= loss;
+        HP.text = "HP: " + health;
+        CheckIfGameOver();
+    }
+    private void CheckIfGameOver()
+    {
+        if (health <= 0)
+        {
+            GameManager.instance.GameOver();
+        }
+    }
+}
